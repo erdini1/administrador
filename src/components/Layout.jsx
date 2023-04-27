d4a07d85d71c9ce5ce122ea093188e35bee8eed3
Dylan Erdini <erdini.dylan@gmail.com>
2023-04-27 00:04:15 -0300

    Agregando estilos al Layout

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -2,13 +2,23 @@ import { Outlet } from "react-router-dom"
 
 function Layout() {
   return (
-    <div>
-        <h1 className="text-6xl font-bold">CRM - React</h1>
-
-        {/* Lo que este debajo de este outltet va a renderizarlo*/}
-        <Outlet/>
+    <div className="md:flex md:min-h-screen">
+      <aside className="md:w-1/4 bg-blue-900 px-5 py-10">
+        <h2 className="text-4xl font-black text-center text-white">CRM - Clientes</h2>
+      </aside>
+      <main className="md:w-3/4 p-10 md:h-screen overflow-scroll">
+        <Outlet />
+      </main>
     </div>
   )
 }
 
 export default Layout
+
+/* 
+CSS
+md:w-1/4 - Quiere decir que esto que tome una de 4 columnas, es decir se va a dividir la pantalla en 4 y y vamos a tomar una de esas 4 dejando el resto para el outlet
+
+overflow-scroll - quiere decir que la parte derecha va a scrolear y la parte izquierda no
+
+*/
