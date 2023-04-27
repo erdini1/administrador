40ba22c8e055c51cdf57400549283bb0d3774654
Dylan Erdini <erdini.dylan@gmail.com>
2023-04-27 17:07:08 -0300

    Añadiendo validacion al formulario

--- src/pages/NuevoCliente.jsx
+++ src/pages/NuevoCliente.jsx
@@ -1,21 +1,37 @@
-import { useNavigate, Form } from "react-router-dom"
+import { useNavigate, Form, useActionData } from "react-router-dom"
 import Formulario from "../components/Formulario"
+import Error from "../components/Error"
 
 //debe se asincrona porque puede ser algo que tarde en traer
+//El formdate va como metodo es decir con () por que esta en el prototype 
 export async function action({request}) {
-  //El formdate va como metodo es decir con () por que esta en el prototype 
+
   const formData = await request.formData()
   const datos = Object.fromEntries(formData)
 
-  console.log(datos)
+  const errores = [];
+  //Validación 
+  if(Object.values(datos).includes("")){
+    errores.push("Todos los campos son obligatorios")
+  }
+
+  if(Object.keys(errores).length){
+    return errores
+  }
+
+  //Retornar datos si hay errores
+  console.log(errores)
 
   return {ok: true}
 }
 
 const NuevoCliente = () => {
 
+  const errores = useActionData()
   const navigate = useNavigate()
 
+
+  console.log(errores)
   return (
     <>
       <h1 className='font-black text-4xl text-blue-900'>Nuevo cliente</h1>
@@ -31,6 +47,8 @@ const NuevoCliente = () => {
       </div>
 
       <div className="bg-white shadow rounded-md md:w-3/4 mx-auto px-5 py-10 mt-20">
+
+        {errores?.length && errores.map((error, i) => <Error key={i}>{error}</Error>)}
 
         <Form
           method="post"
