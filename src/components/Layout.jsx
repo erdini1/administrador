fd8e6b53b0704dc97197d8c7f8d24a89a0186f82
Dylan Erdini <erdini.dylan@gmail.com>
2023-04-27 11:46:35 -0300

    Resaltando la pagina actual

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,16 +1,30 @@
-import { Outlet, Link } from "react-router-dom"
+import { Outlet, Link, useLocation } from "react-router-dom"
 
 function Layout() {
+
+  const location = useLocation()
+  // console.log(location)
+
+
+
   return (
     <div className="md:flex md:min-h-screen">
       <aside className="md:w-1/4 bg-blue-900 px-5 py-10">
         <h2 className="text-4xl font-black text-center text-white">CRM - Clientes</h2>
+        
 
         <nav className="mt-10 ">
-          <Link to="/" className="text-2xl block mt-2 hover:text-blue-300 text-white">Clientes</Link>
-          <Link to="/clientes/nuevo" className="text-2xl block mt-2 hover:text-blue-300 text-white">Nuevo Cliente</Link>
-
+          <Link
+            to="/"
+            // Puedo agregarle template string para cuando este en cliente o nuevo
+            className={`${location.pathname === "/" ? "text-blue-300" : "text-white"} text-2xl block mt-2 hover:text-blue-300`}
+          >Clientes</Link>
+          <Link
+            to="/clientes/nuevo"
+            className={`${location.pathname === "/clientes/nuevo" ? "text-blue-300" : "text-white"} text-2xl block mt-2 hover:text-blue-300`}
+          >Nuevo Cliente</Link>
         </nav>
+        
       </aside>
 
       <main className="md:w-3/4 p-10 md:h-screen overflow-scroll">
